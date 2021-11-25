5cfa4527b1dec8d7b557caced721bea7025f00cb
unknown <hmmt@naver.com>
2021-11-26 02:59:56 +0900

    기본 구조

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,17 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
+router.get('/list', function(req, res, next) {
+
+  let itemList = ["1", "2", "3"];
+  res.render('list', {currentTab:'home', list: itemList});
+});
+router.get('/mypage', function(req, res, next) {
+  res.render('mypage', {currentTab:'mypage'});
+});
+router.get('/recommend', function(req, res, next) {
+  res.render('recommend');
+});
+
 module.exports = router;
